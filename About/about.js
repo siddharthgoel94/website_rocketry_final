28f147f469f54e943bcbca830a8ddca8bfd5e36b
Ashna26-Mittal <ashna.frnds@gmail.com>
2023-10-14 22:49:48 +0530

    updated about page

---
+++ About/about.js
@@ -0,0 +1,16 @@
+const observer=new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            // entry.target.classList.remove("hidden")
+            entry.target.classList.add("show")
+        }
+        else{
+            entry.target.classList.remove("show")
+            // entry.target.classList.add("hidden")
+        }
+    })
+})
+const hiddenElements=document.getElementsByClassName("hidden")
+for (let i=0;i<hiddenElements.length;i++){
+    observer.observe(hiddenElements[i])
+}
